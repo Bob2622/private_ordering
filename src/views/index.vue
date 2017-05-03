4917a6b4c11f470b2fbb080a6ae1a9b219852957
zhangbobo [张波波] <zhangbobo@xiaomi.com>
2017-05-03 10:44:05 +0800

    基本工程架构

---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    hello world
+</div>
+</template>
+<style lang="less" scoped>
+</style>
+<script>
+export default {
+    name: '',
+    props: {
+
+    },
+    data: function () {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted: function () {
+    }
+}
+</script>
